8107d55f5366719f4589166a3ef40785bb484af7
sergioagp <sergiogspereira@gmail.com>
2023-01-24 18:33:31 +0700

    Update IPC

--- tee/tee_entry.c
+++ tee/tee_entry.c
@@ -1,43 +1,51 @@
 #include "tee_entry.h"
-#include "tee_types.h"
+#include "tee_ipc.h"
 #include "tee_ta_api.h"
-
-#define SHARED_MEMORY_ADDRESS 0x40000000
+#include "tee_session.h"
 
 #define UNUSED(x) (void)(x)
 
-void tee_entry(TA_Session *session) {
+void tee_entry(TEE_Operation *operation) {
   TEE_Result ret = TEE_SUCCESS;
+  TEE_Session *session;
+
   // Parse the session structure to determine the operation
-  switch (session->operation) {
+  switch (operation->type) {
     case TEE_OPERATION_OPEN_SESSION:
+      // Create a new session
+      session = create_session();
+      operation->ctx.session_id = session->session_id;
       // Call the TA_OpenSessionEntryPoint function
-      ret = (TEE_Result) TA_OpenSessionEntryPoint((uint32_t) session->params.open_session.param_types,
-                                      (TEE_Param*) session->params.open_session.params,
-                                      (void**) &session->sess_ctx);
+      ret = (TEE_Result) TA_OpenSessionEntryPoint((uint32_t) operation->par.param_types,
+                                      (TEE_Param*) operation->par.params,
+                                      (void**) &session);
       break;
     case TEE_OPERATION_INVOKE_COMMAND:
+      session = get_session(operation->ctx.session_id);
       // Call the TA_InvokeCommandEntryPoint function
-      ret = (TEE_Result) TA_InvokeCommandEntryPoint((void *) session->sess_ctx,
-                                       (uint32_t) session->params.invoke_command.cmd_id,
-                                       (uint32_t) session->params.invoke_command.param_types,
-                                       (TEE_Param*) session->params.invoke_command.params);
+      ret = (TEE_Result) TA_InvokeCommandEntryPoint((void *) session,
+                                       (uint32_t) operation->par.cmd_id,
+                                       (uint32_t) operation->par.param_types,
+                                       (TEE_Param*) operation->par.params);
       break;
     case TEE_OPERATION_CLOSE_SESSION:
+      session = get_session(operation->ctx.session_id);
       // Call the TA_CloseSessionEntryPoint function
-      TA_CloseSessionEntryPoint((void *) session->sess_ctx);
+      TA_CloseSessionEntryPoint((void *) session);
+      // Delete the session id
+      delete_session(operation->ctx.session_id);
       break;
     default:
       ret = TEE_ERROR_BAD_PARAMETERS;
   }
   // Save the return code of the operation
-  session->return_code = ret;
+  operation->ret = ret;
 }
 
 void __tee_entry() {
   // Get the address of the shared memory
-  TA_Session *session = (TA_Session *)SHARED_MEMORY_ADDRESS;
+  TEE_Operation *operation = (TEE_Operation *)TEE_IPC_ADRR;
   // Call the tee_entry function
-  tee_entry(session);
+  tee_entry(operation);
   // Return to sleep
 }
---
+++ tee/tee_ipc.h
@@ -0,0 +1,43 @@
+#ifndef TEE_CLIENT_IPC_H
+#define TEE_CLIENT_IPC_H
+
+#include "tee_api_types.h"
+#include <stdint.h>
+
+#define TEE_IPC_ADRR  0x40000000
+
+#define TEE_OPERATION_OPEN_SESSION    0
+#define TEE_OPERATION_INVOKE_COMMAND  1
+#define TEE_OPERATION_CLOSE_SESSION   2
+
+typedef uint32_t TEE_Result;
+typedef struct {
+    uint32_t session_id;
+    // Other fields to keep track of the context
+} TA_Session_ctx;
+
+typedef struct {
+  int type;
+  union {
+    uint32_t session_id;
+    TEE_UUID tee_uuid;
+  } ctx;
+} TEE_Operation_ctx;
+
+typedef struct {
+  uint32_t cmd_id;
+  uint32_t param_types;
+  TEE_Param params[4];
+} TEE_Operation_params;
+
+typedef struct {
+  int type;
+  union {
+    uint32_t session_id;
+    TEE_UUID tee_uuid;
+  } ctx;
+  TEE_Operation_params par;
+  TEE_Result ret;
+} TEE_Operation;
+
+#endif /* TEE_CLIENT_IPC_H */
